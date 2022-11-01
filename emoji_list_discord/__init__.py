d5fc078d503caf06644168b8661e80fd0440cac9
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2022-11-01 16:03:06 +0900

    Update __init__.py

--- emoji_list_discord/__init__.py
+++ emoji_list_discord/__init__.py
@@ -1,3 +1,86 @@
+__version__ = 0.1.1
+
+# Smileys and people
 face_positive = [u'\U0001f600',u'\U0001f601',u'\U0001f602',u'\U0001f923',u'\U0001f603',u'\U0001f604',u'\U0001f605',u'\U0001f606',u'\U0001f609',u'\U0001f60a',u'\U0001f60b',u'\U0001f60e',u'\U0001f60d',u'\U0001f618',u'\U0001f617',u'\U0001f619',u'\U0001f61a',u'\u263a',u'\U0001f642',u'\U0001f917',u'\U0001f929']
 face_neutral = [u'\U0001f914',u'\U0001f928',u'\U0001f610',u'\U0001f611',u'\U0001f636',u'\U0001f644',u'\U0001f60f',u'\U0001f623',u'\U0001f625',u'\U0001f62e',u'\U0001f910',u'\U0001f62f',u'\U0001f62a',u'\U0001f62b',u'\U0001f634',u'\U0001f60c',u'\U0001f61b',u'\U0001f61c',u'\U0001f61d',u'\U0001f924',u'\U0001f612',u'\U0001f613',u'\U0001f614',u'\U0001f615',u'\U0001f643',u'\U0001f911',u'\U0001f632']
 face_negative = [u'\u2639',u'\U0001f641',u'\U0001f616',u'\U0001f61e',u'\U0001f61f',u'\U0001f624',u'\U0001f622',u'\U0001f62d',u'\U0001f626',u'\U0001f627',u'\U0001f628',u'\U0001f629',u'\U0001f92f',u'\U0001f62c',u'\U0001f630',u'\U0001f631',u'\U0001f633',u'\U0001f92a',u'\U0001f635',u'\U0001f621',u'\U0001f620',u'\U0001f92c']
+face_sick = []
+face_role = []
+face_fantasy = []
+face_cat = []
+face_monkey = []
+person = []
+person_role = []
+person_fantasy = []
+person_gesture = []
+person_activity = []
+person_sport
+family = []
+body = []
+emotion = []
+clothing = []
+
+# Animals and nature
+
+animals_mammal = []
+animal_bird = []
+animal_amphibian = []
+animal_reptile = []
+animal_marine = []
+animal_bug = []
+plant_flower = []
+plant_other = []
+
+# Travel and places
+
+place_map = []
+place_geographic = []
+place_building = []
+place_religious = []
+place_other = []
+transport_ground = []
+transport_water = []
+transport_air = []
+hotel = []
+time = []
+weather = []
+
+# Activities
+
+event = []
+award_medal = []
+sport = []
+game = []
+
+# Objects
+
+sound = []
+music = []
+musical_instrument = []
+phone = []
+computer = []
+light_video = []
+book_paper = []
+money = []
+mail = []
+writing = []
+office = []
+lock = []
+tool = []
+medical = []
+other_object = []
+
+# Symbols
+
+transport_sign = []
+warning = []
+arrow = []
+religion = []
+av_symbol = []
+other_symbol = []
+keycap = []
+alphanum = []
+geometric = []
+flag = []
+country_flag = []
+subdivision_flag = []
