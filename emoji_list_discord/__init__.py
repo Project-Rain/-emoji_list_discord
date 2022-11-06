1a4b35f546f72ca7e9f90293f33b3b737b7afecf
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2022-11-07 01:36:57 +0900

    Update __init__.py

--- emoji_list_discord/__init__.py
+++ emoji_list_discord/__init__.py
@@ -12,7 +12,7 @@ from emoji_list_discord.Symbols import *
 
 all_Smileys_and_people = list(set(face_positive+face_neutral+face_negative+face_sick+face_role+face_fantasy+face_cat+face_monkey+person+person_role+person_fantasy+person_gesture+person_activity+person_sport+family+body+emotion+clothing))
 all_Animals_and_nature = list(set(animals_mammal+animal_bird+animal_amphibian+animal_reptile+animal_marine+animal_bug+plant_flower+plant_other))
-all_Food_and_drink = list(set())
+all_Food_and_drink = list(set(food_fruit+food_vegetable+food_prepared+food_asian+food_sweet+drink+dishware))
 all_Travel_and_places = list(set(place_map+place_geographic+place_building+place_religious+place_other+transport_ground+transport_water+transport_air+hotel))
 all_Activities = list(set(event+award_medal+sport+game))
 all_Objects = list(set(sound+music+musical_instrument+phone+computer+light_video+book_paper+money+mail+writing+office+lock+tool+medical+other_object))
